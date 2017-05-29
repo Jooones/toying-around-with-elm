c4d4c055b1abd2b2abb80a869e82de9435036642
Jooones <jonathanV@bitbucket.org>
2017-05-29 22:05:21 +0200

    playing with forms, did 3 of the 4 exercises

--- my-form.elm
+++ my-form.elm
@@ -1,8 +1,10 @@
 module Main exposing (..)
 
+import Char exposing (isDigit)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onInput)
+import String exposing (all, any, length, toLower, toUpper)
 
 
 main : Program Never Model Msg
@@ -16,6 +18,7 @@ main =
 
 type alias Model =
     { name : String
+    , age : String
     , password : String
     , passwordAgain : String
     }
@@ -23,7 +26,7 @@ type alias Model =
 
 model : Model
 model =
-    Model "" "" ""
+    Model "" "" "" ""
 
 
 
@@ -32,6 +35,7 @@ model =
 
 type Msg
     = Name String
+    | Age String
     | Password String
     | PasswordAgain String
 
@@ -41,6 +45,9 @@ update msg model =
     case msg of
         Name name ->
             { model | name = name }
+
+        Age age ->
+            { model | age = age }
 
         Password password ->
             { model | password = password }
@@ -57,8 +64,12 @@ view : Model -> Html Msg
 view model =
     div []
         [ input [ type_ "text", placeholder "Name", onInput Name ] []
+        , input [ type_ "text", placeholder "Age", onInput Age ] []
         , input [ type_ "password", placeholder "Password", onInput Password ] []
         , input [ type_ "password", placeholder "Re-enter Password", onInput PasswordAgain ] []
+
+        -- , [ button [ onClick Submit ] [ text "Submit" ]
+        -- TODO only show messages when submitted!
         , viewValidation model
         ]
 
@@ -67,9 +78,19 @@ viewValidation : Model -> Html msg
 viewValidation model =
     let
         ( color, message ) =
-            if model.password == model.passwordAgain then
-                ( "green", "OK" )
-            else
+            if model.password /= model.passwordAgain then
                 ( "red", "Passwords do not match!" )
+            else if length model.password < 9 then
+                ( "red", "Passwords must be longer than 8 characters!" )
+            else if toLower model.password == model.password then
+                ( "red", "Passwords must contain at least one upper case character!" )
+            else if toUpper model.password == model.password then
+                ( "red", "Passwords must contain at least one lower case character!" )
+            else if not (any isDigit model.password) then
+                ( "red", "Passwords must contain at least one numeric character!" )
+            else if not (all isDigit model.age) then
+                ( "red", "Age must contain only digits!" )
+            else
+                ( "green", "OK" )
     in
     div [ style [ ( "color", color ) ] ] [ text message ]
