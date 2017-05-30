5678d48ac38da15ec0aa89d1c39ba31eb760a9df
Jooones <jonathanV@bitbucket.org>
2017-05-30 21:39:41 +0200

    complete forn task

--- my-form.elm
+++ my-form.elm
@@ -3,7 +3,7 @@ module Main exposing (..)
 import Char exposing (isDigit)
 import Html exposing (..)
 import Html.Attributes exposing (..)
-import Html.Events exposing (onInput)
+import Html.Events exposing (onClick, onInput)
 import String exposing (all, any, length, toLower, toUpper)
 
 
@@ -21,12 +21,14 @@ type alias Model =
     , age : String
     , password : String
     , passwordAgain : String
+    , messageColor : String
+    , message : String
     }
 
 
 model : Model
 model =
-    Model "" "" "" ""
+    Model "" "" "" "" "" ""
 
 
 
@@ -38,6 +40,7 @@ type Msg
     | Age String
     | Password String
     | PasswordAgain String
+    | Submit
 
 
 update : Msg -> Model -> Model
@@ -55,6 +58,22 @@ update msg model =
         PasswordAgain password ->
             { model | passwordAgain = password }
 
+        Submit ->
+            if model.password /= model.passwordAgain then
+                { model | messageColor = "red", message = "Passwords do not match!" }
+            else if length model.password < 9 then
+                { model | messageColor = "red", message = "Passwords must be longer than 8 characters!" }
+            else if toLower model.password == model.password then
+                { model | messageColor = "red", message = "Passwords must contain at least one upper case character!" }
+            else if toUpper model.password == model.password then
+                { model | messageColor = "red", message = "Passwords must contain at least one lower case character!" }
+            else if not (any isDigit model.password) then
+                { model | messageColor = "red", message = "Passwords must contain at least one numeric character!" }
+            else if not (all isDigit model.age) then
+                { model | messageColor = "red", message = "Age must contain only digits!" }
+            else
+                { model | messageColor = "green", message = "Like a baws!" }
+
 
 
 -- VIEW
@@ -63,34 +82,12 @@ update msg model =
 view : Model -> Html Msg
 view model =
     div []
-        [ input [ type_ "text", placeholder "Name", onInput Name ] []
-        , input [ type_ "text", placeholder "Age", onInput Age ] []
-        , input [ type_ "password", placeholder "Password", onInput Password ] []
-        , input [ type_ "password", placeholder "Re-enter Password", onInput PasswordAgain ] []
-
-        -- , [ button [ onClick Submit ] [ text "Submit" ]
-        -- TODO only show messages when submitted!
-        , viewValidation model
+        [ div []
+            [ input [ type_ "text", placeholder "Name", onInput Name ] []
+            , input [ type_ "text", placeholder "Age", onInput Age ] []
+            , input [ type_ "password", placeholder "Password", onInput Password ] []
+            , input [ type_ "password", placeholder "Re-enter Password", onInput PasswordAgain ] []
+            ]
+        , button [ onClick Submit ] [ text "Submit" ]
+        , div [ style [ ( "color", model.messageColor ) ] ] [ text model.message ]
         ]
-
-
-viewValidation : Model -> Html msg
-viewValidation model =
-    let
-        ( color, message ) =
-            if model.password /= model.passwordAgain then
-                ( "red", "Passwords do not match!" )
-            else if length model.password < 9 then
-                ( "red", "Passwords must be longer than 8 characters!" )
-            else if toLower model.password == model.password then
-                ( "red", "Passwords must contain at least one upper case character!" )
-            else if toUpper model.password == model.password then
-                ( "red", "Passwords must contain at least one lower case character!" )
-            else if not (any isDigit model.password) then
-                ( "red", "Passwords must contain at least one numeric character!" )
-            else if not (all isDigit model.age) then
-                ( "red", "Age must contain only digits!" )
-            else
-                ( "green", "OK" )
-    in
-    div [ style [ ( "color", color ) ] ] [ text message ]
